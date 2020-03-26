c92f57e84e420a8b2a2e13dc1fe6225ca359e4ed
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-03-26 22:38:29 +0600

    Running 1st test (3)

--- install.sh
+++ install.sh
@@ -7,48 +7,133 @@ message()
 	echo
 }
 
-message "updating default timeout to 30 minutes"
-sudo echo "Defaults passwd_timeout=30" >> /etc/sudoers
+conf_message()
+{
+	echo
+	printf "(@SED) Do you wish to $1? (Y/n)"
+}
 
-message "upgrading the system"
-sudo pacman -Syyu
-sudo pacman -S yay
+while true; do
+	conf_message "update default timeout and define custom keyboard shortcut" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "updating default timeout to 30 minutes"
+			sudo echo "Defaults passwd_timeout=30" >> /etc/sudoers
 
-message "installing necessary tools"
-yay -S zsh tlp neofetch google-chrome brave vlc intellij code qbittorrent xdman handbrake fish sublime-text gimp libreoffice-still mlocate timeshift
+			message "defining custom keyboard shortcut"
+			python custom_keyboard_shortcut.py 'open Terminal' 'gnome-terminal' '<Control><Alt>T'
+			python custom_keyboard_shortcut.py 'open File manager' 'nautilus' '<Super>E'
+			python custom_keyboard_shortcut.py 'open Sublime-text' 'subl' '<Control><Alt>S'
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
 
-message "configuring the tools"
-alias ll="ls -la"
-sudo tlp start
+while true; do
+	conf_message "update the system" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "upgrading the system"
+			sudo pacman -Syyu
+			sudo pacman -S yay
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
 
-message "configuring sublime-text"
-mkdir ~/Documents/my_code
-touch ~/Documents/my_code/input.txt
-touch ~/Documents/my_code/output.txt
-if [ -d ~/.config/sublime-text-3 ]; then
-	rm -rf ~/.config/sublime-text-3/
-	message "sublime-text's existing config deleted"
-fi
-message "copying new config files"
-cp -r config-files/sublime-text-3/ ~/.config
+while true; do
+	conf_message "install & configure the necessary tools" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "installing necessary tools"
+			yay -S zsh tlp neofetch google-chrome brave vlc intellij code qbittorrent xdman handbrake fish sublime-text gimp libreoffice-still mlocate timeshift
+	       	
+	       	message "configuring the tools"
+			alias ll="ls -la"
 
-message "installing & configuring virtualization"
-yay -S virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
+			#For laptop
+			sudo tlp start
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
 
-message "copying themes"
-cp -r config-files/Themes_and_Icons ~/
+while true; do
+	conf_message "configure sublime-text" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "configuring sublime-text"
+			mkdir ~/Documents/my_code
+			touch ~/Documents/my_code/input.txt
+			touch ~/Documents/my_code/output.txt
+			if [ -d ~/.config/sublime-text-3 ]; then
+				rm -rf ~/.config/sublime-text-3/
+				message "sublime-text's existing config deleted"
+			fi
+			message "copying new config files"
+			cp -r config-files/sublime-text-3/ ~/.config
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
 
-message "changing default shell to zsh"
-chsh -s /bin/zsh
-sudo chsh -s /bin/zsh
+while true; do
+	conf_message "install & configure virtualization" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "installing & configuring virtualization"
+			yay -S virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
+			sudo systemctl enable libvirtd.service
+			sudo systemctl start libvirtd.service
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
 
-message "configuring zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"		
-git clone https://github.com/zsh-users/zsh-autosuggestions
-mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-rm -rf ~/.zshrc
-cp config-files/.zshrc ~/
-message "Everything executed without any error"
+while true; do
+	conf_message "configure Themes_and_Icons" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "copying themes & icons files"
+			cp -r config-files/Themes_and_Icons ~/
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
+
+while true; do
+	conf_message "Change default shell and set custom configuration for it" 
+    read -p "" yn
+    case $yn in
+        [Yy]*|"" )
+			message "changing default shell to zsh"
+			chsh -s /bin/zsh
+			sudo chsh -s /bin/zsh
+
+			message "configuring zsh"
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"		
+			git clone https://github.com/zsh-users/zsh-autosuggestions
+			mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+			rm -rf ~/.zshrc
+			cp config-files/.zshrc ~/
+	       	break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Y/y or N/n as yes or no.";;
+    esac
+done
+
+
+message "Everything executed without any error :)"
