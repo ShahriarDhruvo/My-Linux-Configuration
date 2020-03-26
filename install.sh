725c01aa152a056af434ac4f6ffebf5ef3deea8f
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-03-26 20:47:29 +0600

    Running 1st test (2)

--- install.sh
+++ install.sh
@@ -1,46 +1,54 @@
 #!/bin/sh
 
-echo "UPGRADING THE SYSTEM...."
+message()
+{
+	echo
+	echo "(@sed) $1...." | tr a-z A-Z
+	echo
+}
+
+message "updating default timeout to 30 minutes"
+sudo echo "Defaults passwd_timeout=30" >> /etc/sudoers
+
+message "upgrading the system"
 sudo pacman -Syyu
 sudo pacman -S yay
 
-echo "INSTALLING NECESSARY TOOLS...."
+message "installing necessary tools"
 yay -S zsh tlp neofetch google-chrome brave vlc intellij code qbittorrent xdman handbrake fish sublime-text gimp libreoffice-still mlocate timeshift
 
-echo "CONFIGURING THE TOOLS...."
+message "configuring the tools"
 alias ll="ls -la"
 sudo tlp start
 
-echo "CONFIGURING SUBLIME-TEXT...."
-if [ ! -d ~/Documents/my_code ]; then
-	mkdir ~/Documents/my_code
-fi
-if [ ! -f ~/Documents/my_code/input.txt ]; then
-	touch ~/Documents/my_code/input.txt
-fi
-if [ ! -f ~/Documents/my_code/output.txt ]; then
-	touch ~/Documents/my_code/output.txt
-fi
-if [ ! -x "$(command -v svn)" ]; then
-  sudo pacman -S subversion
-fi
+message "configuring sublime-text"
+mkdir ~/Documents/my_code
+touch ~/Documents/my_code/input.txt
+touch ~/Documents/my_code/output.txt
 if [ -d ~/.config/sublime-text-3 ]; then
 	rm -rf ~/.config/sublime-text-3/
+	message "sublime-text's existing config deleted"
 fi
-cp config-files/sublime-text-3/ ~/.config
+message "copying new config files"
+cp -r config-files/sublime-text-3/ ~/.config
 
-echo "CONFIGURING ZSH...."
+message "installing & configuring virtualization"
+yay -S virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+message "copying themes"
+cp -r config-files/Themes_and_Icons ~/
+
+message "changing default shell to zsh"
+chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
+
+message "configuring zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"		
 git clone https://github.com/zsh-users/zsh-autosuggestions
 mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 rm -rf ~/.zshrc
 cp config-files/.zshrc ~/
-
-echo "INSTALLING & CONFIGURING VIRTUALIZATION...."
-yay -S virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
-
-echo "CONFIGURING THEMES...."
-cp config-files/Themes_and_Icons ~/
+message "Everything executed without any error"
